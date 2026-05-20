17438a6c54ce2629e87444e3c5db6a42497eab2a
Mahmoud Gamal <mahmoudgemmy581@gmail.com>
2026-05-20 17:09:14 +0300

    chore: configure global validation pipe, environment variables, and MongoDB connection

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,29 @@
-import {Module} from '@nestjs/common';
+import {Module, ValidationPipe} from '@nestjs/common';
+import {ConfigModule, ConfigService} from "@nestjs/config";
+import {MongooseModule} from "@nestjs/mongoose";
 
 @Module({
-    imports: [],
+    imports: [
+        ConfigModule.forRoot({
+            isGlobal: true,
+        }),
+        MongooseModule.forRootAsync({
+            useFactory: (configService: ConfigService) => ({
+                uri: configService.getOrThrow('MONGODB_URI')
+            }),
+            inject: [ConfigService]
+        })
+    ],
+    providers: [
+        {
+            provide: 'APP_PIPE',
+            useValue: new ValidationPipe({
+                whitelist: true,
+                forbidNonWhitelisted: true,
+                transform: true,
+            }),
+        }
+    ]
 })
 export class AppModule {
 }
